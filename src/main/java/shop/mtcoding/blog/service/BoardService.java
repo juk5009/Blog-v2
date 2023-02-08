2437ee191c96edcee3a541a297e1d0ce51d2efd8
Kjuk <juk6340@gmail.com>
2023-02-08 14:58:31 +0900

    detail design change

--- src/main/java/shop/mtcoding/blog/service/BoardService.java
+++ src/main/java/shop/mtcoding/blog/service/BoardService.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 import shop.mtcoding.blog.dto.board.BoardReq.BoardSaveReqDto;
 import shop.mtcoding.blog.dto.board.BoardReq.BoardUpdateReqDto;
 import shop.mtcoding.blog.handler.ex.CustomApiException;
+import shop.mtcoding.blog.handler.ex.CustomException;
 import shop.mtcoding.blog.model.Board;
 import shop.mtcoding.blog.model.BoardRepository;
 
